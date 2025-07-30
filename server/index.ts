dfc162258feb418eff0001a9a9cd000f34c079ec
Mr WTF <125758201+Omarjourney@users.noreply.github.com>
2025-07-30 14:44:14 -0700

    Add CORS middleware

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(',')
+  })
+);
 
 app.use('/auth', authRoutes);
 app.use('/users', usersRoutes);
